a8d6c42fcea0776dc21f1d1ef9e8d65b3b021385
adamjonas <jonas@flatironschool.com>
2018-06-14 21:26:33 -0400

    add import and rm tests

--- test/index_test.py
+++ test/index_test.py
@@ -3,22 +3,6 @@ from ipynb.fs.full.index import *
 
 class FieldElementTest(TestCase):
 
-    def test_add(self):
-        a = FieldElement(2, 31)
-        b = FieldElement(15, 31)
-        self.assertEqual(a+b, FieldElement(17, 31))
-        a = FieldElement(17, 31)
-        b = FieldElement(21, 31)
-        self.assertEqual(a+b, FieldElement(7, 31))
-
-    def test_sub(self):
-        a = FieldElement(29, 31)
-        b = FieldElement(4, 31)
-        self.assertEqual(a-b, FieldElement(25, 31))
-        a = FieldElement(15, 31)
-        b = FieldElement(30, 31)
-        self.assertEqual(a-b, FieldElement(16, 31))
-
     def test_mul(self):
         a = FieldElement(24, 31)
         b = FieldElement(19, 31)
@@ -30,39 +14,3 @@ class FieldElementTest(TestCase):
         a = FieldElement(5, 31)
         b = FieldElement(18, 31)
         self.assertEqual(a**5 * b, FieldElement(16, 31))
-
-    def test_div(self):
-        a = FieldElement(3, 31)
-        b = FieldElement(24, 31)
-        self.assertEqual(a/b, FieldElement(4, 31))
-        a = FieldElement(17, 31)
-        self.assertEqual(a**-3, FieldElement(29, 31))
-        a = FieldElement(4, 31)
-        b = FieldElement(11, 31)
-        self.assertEqual(a**-4*b, FieldElement(13, 31))
-
-class PointTest(TestCase):
-
-    def test_on_curve(self):
-        with self.assertRaises(RuntimeError):
-            Point(x=-2, y=4, a=5, b=7)
-        # these should not raise an error
-        Point(x=3, y=-7, a=5, b=7)
-        Point(x=18, y=77, a=5, b=7)
-
-    def test_add0(self):
-        a = Point(x=None, y=None, a=5, b=7)
-        b = Point(x=2, y=5, a=5, b=7)
-        c = Point(x=2, y=-5, a=5, b=7)
-        self.assertEqual(a+b, b)
-        self.assertEqual(b+a, b)
-        self.assertEqual(b+c, a)
-
-    def test_add1(self):
-        a = Point(x=3, y=7, a=5, b=7)
-        b = Point(x=-1, y=-1, a=5, b=7)
-        self.assertEqual(a+b, Point(x=2, y=-5, a=5, b=7))
-
-    def test_add2(self):
-        a = Point(x=-1, y=1, a=5, b=7)
-        self.assertEqual(a+a, Point(x=18, y=-77, a=5, b=7))
